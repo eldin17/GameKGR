d7b48a5cf42c3192ce2e81adc2456a387d1e82cb
eldin17 <82153592+eldin17@users.noreply.github.com>
2021-04-21 00:04:42 +0200

    Level1Template

--- ShipGame/Assets/Scripts/Player/PlayerMovement.cs
+++ ShipGame/Assets/Scripts/Player/PlayerMovement.cs
@@ -8,8 +8,9 @@ namespace ShipGame.Player
 	{  
 	    private void Update()
 	    {
-			this.transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, 0) * 30 * Time.deltaTime;
-			this.transform.position += new Vector3(0, 0, 5) * 10 * Time.deltaTime;
+			this.transform.Rotate(0, Input.GetAxis("Horizontal") * 150 * Time.deltaTime, 0);
+			//this.transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, 0) * 30 * Time.deltaTime;
+			//this.transform.position += new Vector3(0, 0, 5) * 10 * Time.deltaTime;
 		}
 	}
 }
